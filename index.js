0e306ca5f75ceaf056621c8acd59ef0fc2b3df59
Mohamd Imad <medoimad@hotmail.com>
2022-08-15 09:07:38 -0400

    create three documents html, css and js and place the jquery link in the html document

---
+++ index.js
@@ -0,0 +1,11 @@
+// The jQuery code inside the ready keyword. this is helpful, because it will
+// execute the code once jQuery has been loaded properly The .ready() method offers a way to run
+// JavaScript code as soon as the page's Document Object Model (DOM) becomes safe to manipulate.
+
+$(document).ready(function() {
+  $("h1").css("color", "red");
+
+
+
+// These brackets are for the .ready() method
+});
