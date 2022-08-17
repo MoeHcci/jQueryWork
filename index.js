9c967b12fde2389db355f35efed81b5cd23a695b
Mohamd Imad <medoimad@hotmail.com>
2022-08-16 23:19:04 -0400

    Add -edtiting an attribute -editing a text and html -editting an attribute

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // The jQuery code inside the ready keyword. this is helpful, because it will
 // execute the code once jQuery has been loaded properly The .ready() method offers a way to run
 // JavaScript code as soon as the page's Document Object Model (DOM) becomes safe to manipulate.
+// This is just a paractice sheet. But we need to keep in mind .js is not for styles
+// We can use $ or jQuery to call out jQuery instead of the lengthy Villa JS
 
 $(document).ready(function() {
 
@@ -13,7 +15,7 @@ $(document).ready(function() {
   // Change the style of a class
   // $(".myClass") is the same as document.getElementsByClassName("myClass")
   // This will produce an array aka object
-  $(".myClass").css("border", "3px solid red");
+  jQuery(".myClass").css("border", "3px solid red");
 
   // Change the style of an id & multiple CSS properties
   // $("#praragraph2") is the same as document.getElementsByIdName("praragraph2")
@@ -26,6 +28,29 @@ $(document).ready(function() {
 // It's important to note that this method does not replace a class.
 // It simply adds the class, appending it to any which may already be assigned to the elements.
 $("p").addClass("praragraph2");
+
+
+
+// To edit the an attribute using jQuery, you need a two inputs while using the .attr() function.
+// The example below edits all the images in the html pagefirst image
+// $("img").attr("src", "violin.jpg");
+
+
+// To edit the an attribute using jQuery, you need a two inputs while using the .attr() function.
+// The example below edits specific images in the html pagefirst image
+$("#musicalInstrument").attr("src", "violin.jpg");
+
+
+// Editing an element's text
+$("#praragraph1").text("hi");
+
+
+// Editing an element's text + html of its content
+$(".myClass").html("<em>hello click me</em>")
+
+
+
+
 
   // These brackets are for the .ready() method
 });
